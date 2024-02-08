a1fadc1053965461ca8659dba313ec3c2da9fb3b
NIHAJ-RARBA <altairahad001@gmail.com>
2024-02-08 19:33:59 +0600

    Setup Done ig

---
+++ createDB.sql
@@ -0,0 +1,3 @@
+CREATE TABLE schools (
+    id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100)
+);
---
+++ server.js
@@ -0,0 +1,47 @@
+
+import express from 'express';
+import pool from './DB.js';
+const port = 8000;
+
+const app = express();
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     res.sendStatus(200);
+// });
+
+
+
+//routes
+app.get('/', async (req, res) => {
+    try {
+        const data = await pool.query('SELECT * FROM schools')
+        res.status(200).send(data.rows)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
+app.post('/', async (req, res) => {
+    const { name, address } = req.body
+    try {
+        if (address) { // Check if address is provided
+            await pool.query('INSERT INTO schools (name, address) VALUES ($1, $2)', [name, address])
+            res.status(200).send({ message: "Successfully added school" })
+        } else {
+            res.status(400).send({ message: "Address is required" })
+        }
+
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`);
+});
+
+
+export default app;
