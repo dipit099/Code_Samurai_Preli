af78210e780de71e79b268569692f279bd5fea6a
Dipit Saha <sdipit099@gmail.com>
2024-02-09 20:05:42 +0600

    your message here

--- DB.js
+++ DB.js
@@ -2,21 +2,21 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
-const pool = new Pool({
-    host: 'postgres',
-    port: 5432,
-    user: 'db',
-    password: 'db',
-    database: 'db'
-});
-
-
 // const pool = new Pool({
-//     host: 'aws-0-ap-southeast-1.pooler.supabase.com',
+//     host: 'postgres',
 //     port: 5432,
-//     user: 'postgres.ygbtzjrgwxgjapzbqfjh',
-//     password: 'KLKrGwXtUrPGtk3E',
-//     database: 'postgres'
+//     user: 'db',
+//     password: 'db',
+//     database: 'db'
 // });
+
+
+const pool = new Pool({
+    host: 'aws-0-ap-southeast-1.pooler.supabase.com',
+    port: 5432,
+    user: 'postgres.ygbtzjrgwxgjapzbqfjh',
+    password: 'KLKrGwXtUrPGtk3E',
+    database: 'postgres'
+});
 
 export default pool;
